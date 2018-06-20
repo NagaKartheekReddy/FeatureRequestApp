4de7bf239b451538607e55ebce5bcd54252004ef
Naga Kartheek Reddy Kona <NK00507057@techmahindra.com>
2018-06-20 15:04:56 +0530

    secondcommit

--- AssignmentFeatureRequestApp/RequestAppVirtualenv/RequestAppContents/__init__.py
+++ AssignmentFeatureRequestApp/RequestAppVirtualenv/RequestAppContents/__init__.py
@@ -2,4 +2,4 @@ from flask import Flask # importing Flask class from flask package
 
 object_flask=Flask(__name__) #creating flask object
 
-from RequestAppContents import requestappviews, featurerequestmanager, featurerequestservice, model
+from RequestAppContents import requestappviews, featurerequestmanager, featurerequestservice, model 
